d17219b25a7a14ddd517c76ce587456c764117b5
cstlore <lorekornyachka@gmail.com>
2025-07-15 01:17:08 +0400

    load

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -14,7 +14,6 @@ const normalize = (response: { docs: any[] }): Movie[] => {
     })
 }
 export const sendRequest = async (filters: Filters): Promise<Movie[]> => {
-    console.log(filters)
     const response = await fetch('/v1.4/movie?' + new URLSearchParams({
         'page': filters.page.toString(),
         'limit': filters.limit.toString(),
@@ -24,8 +23,28 @@ export const sendRequest = async (filters: Filters): Promise<Movie[]> => {
     }).toString(), {
         method: 'GET',
         headers: {
+            'Content-Type': 'application/json',
             "X-API-KEY": KINOPOISK_API_KEY,
         },
     })
     return normalize(await response.json())
+}
+export const getMovieDetail = async (
+    kinopoiskId: string
+): Promise<Movie> => {
+    const url = `/v1.4/movie/${kinopoiskId}`;
+    const res = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': KINOPOISK_API_KEY,
+        },
+    })
+    const data = await res.json()
+    return {
+        id: data.id.toString(),
+        title: data.name ?? data.alternativeName ?? "—",
+        year: data.year,
+        rating: data.rating?.kp,
+        posterUrl: data.poster?.url
+    }
 }
--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getMovieDetail} from "../api/Api";
+import {useParams} from "react-router-dom";
+import {Movie} from "../api/Movie";
+import undefinedPoster from "../images/undefinedPoster.png";
 
 
 const MovieDetail: React.FC = () => {
+    const {id} = useParams<{
+        id: string;
+    }>();
+    const [movie, setMovie] = useState<Movie>({
+        id: '',
+        title: '',
+        year: 0
+    })
+    useEffect(() => {
+        getMovieDetail(id!).then((res) => {
+            setMovie(res)
+        })
+    }, []);
     return (
-        <div>
-
+        <div className="w-[100%] flex flex-col items-center justify-center">
+            <img className="h-[60vh]" src={movie.posterUrl ?? undefinedPoster} alt={movie.title}/>
+            <p className="text-center text-[30px] font-medium mt-[4px]">{movie.title}</p>
+            <p className="text-center text-gray-500 text-[14px] font-medium">{movie.year}</p>
+            <p className="text-[24px] mt-[20px]">🌟{movie.rating === 0 ? '-' : movie.rating}</p>
+            <p className="text-gray-400">Рейтинг <span className="text-orange-500">Кино</span><span
+                className="text-neutral-600">поиск</span></p>
         </div>
     )
 }
